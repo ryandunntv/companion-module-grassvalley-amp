156bc1f9eb4302f5e03afaa904ef39dc67f9d494
Justin Osborne <justin@eblah.com>
2022-10-20 12:59:06 -0500

    v3 support

---
+++ upgrades.js
@@ -0,0 +1,31 @@
+import { CreateConvertToBooleanFeedbackUpgradeScript } from '@companion-module/base'
+
+export default [
+    CreateConvertToBooleanFeedbackUpgradeScript({
+        transport: {
+            bg: 'bgcolor',
+            fg: 'color',
+            text: 'text'
+        }
+    }),
+
+    function(context, props) {
+        props.actions.forEach(x => {
+            if (x.options && 'clipdd' in x.options) {
+                if ('clip' in x.options && x.options.clip == '') {
+                    x.options.clip = x.options.clipdd;
+                }
+
+                delete x.options.clipdd;
+            }
+
+            new_actions.push(x)
+        });
+
+        return {
+            updatedConfig: null,
+            updatedActions: new_actions,
+            updatedFeedbacks: [],
+        }
+    }
+]
